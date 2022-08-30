fbdc2531fd6ba146c3331ca1952a9ccb7ed6e073
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-08-29 22:15:33 -0300

    feat: created an error class to error treating

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import { routes } from './routes/routes';
+import { AppError } from './errors/app.errors';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+// Route Middleware
 app.use(routes);
 
+// Error Middleware
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal Server Error' });
+});
+
+// Server listening
 const PORT = 4444;
 
 app.listen(PORT, () => {
