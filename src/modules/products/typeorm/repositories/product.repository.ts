c237dac7ed08490efb17638d074cffa327ab13fb
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-09-01 00:14:52 -0300

    feat: added an entity repository for products

---
+++ src/modules/products/typeorm/repositories/product.repository.ts
@@ -0,0 +1,11 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Product } from '../entities/product.entity';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({ where: { name } });
+
+    return product;
+  }
+}
